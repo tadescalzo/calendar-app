674dc621b4d279f571974186db97cea12a2543cc
tadescalzo <ta.descalzo@gmail.com>
2023-04-21 21:28:19 -0300

    instalacion de calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,46 @@
+import { Navbar } from "../components/Navbar"
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import enUS from 'date-fns/locale/en-US'
+import { addHours,format,parse,startOfWeek,getDay } from "date-fns"
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Mi cumple',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(),2),
+  bgColor: '#fafafa',
+  user:{
+    _id:'123',
+    name:'Nacho'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+    <>
+    <Navbar/>
+
+    <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc( 100vh - 80px )' }}
+    />
+
+    </>
   )
 }
