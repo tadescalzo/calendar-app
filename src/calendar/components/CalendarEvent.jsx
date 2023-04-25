46b3e65ac3aff34ff7b1a0d35524e1aae5a722e8
tadescalzo <ta.descalzo@gmail.com>
2023-04-24 22:57:06 -0300

    configuracion del calendario

---
+++ src/calendar/components/CalendarEvent.jsx
@@ -0,0 +1,19 @@
+
+
+export const CalendarEvent = ({ event }) => {
+
+    /* const {title, user} = event; */
+    const { title, user } = event
+
+
+  return (
+    <>
+        <strong>{title} </strong>
+        <span>- {user.name} </span>
+    </>
+    
+  )
+}
+
+
+
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,26 +1,18 @@
 import { Navbar } from "../components/Navbar"
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import enUS from 'date-fns/locale/en-US'
-import { addHours,format,parse,startOfWeek,getDay } from "date-fns"
+import { addHours } from "date-fns";
+import { getMessagesES, localizer } from "../../helpers";
+import { CalendarEvent } from "../components/CalendarEvent";
 
-const locales = {
-  'en-US': enUS,
-}
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+
 
 const events = [{
   title: 'Mi cumple',
   notes: 'Hay que comprar el pastel',
   start: new Date(),
-  end: addHours(new Date(),2),
+  end: addHours(new Date(),1),
   bgColor: '#fafafa',
   user:{
     _id:'123',
@@ -29,16 +21,53 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) =>{
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return{
+      style
+    }
+  }
+
+  const onDoubleClick = ( event ) => {
+    console.log({doubleClick:event})
+  }
+
+  const onSelect = ( event ) => {
+    console.log({click:event})
+  }
+
+  const onViewChange = ( event ) => {
+    console.log({ viewChanged: event })
+  }
+
+
   return (
     <>
     <Navbar/>
 
     <Calendar
+    culture="es"
       localizer={localizer}
       events={events}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 'calc( 100vh - 80px )' }}
+      messages={ getMessagesES() }
+      eventPropGetter={ eventStyleGetter }
+      components={{
+        event: CalendarEvent
+      }}
+      onDoubleClickEvent={onDoubleClick}
+      onSelectEvent={onSelect}
+      onView={onViewChange}
     />
 
     </>
